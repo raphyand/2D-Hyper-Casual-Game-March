ce59ffb27db18ea1fa06bfc4b8c80ce3adbff24e
raphyand <raphyand@gmail.com>
2022-03-25 00:52:39 -0700

    Added Teleporter Prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,41 +7,88 @@ public class PlayerMovement : MonoBehaviour
 {
 
      [SerializeField] private float moveSpeed = 2.0f;
+     [SerializeField] private float jumpHeight = 5.0f;
      Rigidbody2D myRigidBody2D;
-
+     CircleCollider2D myCircleCollider2D;
+     int numJumps = 2;
+     int jumpsLeft;
+     [SerializeField]
+     private bool canJump;
+     [SerializeField]
+     LayerMask canJumpOn;
 
      // Vector of the previous move input.  Used for determining
      // what direction the object should face when movement stops.
      // NOTE: Taken from Baklava, could adapt to something else.
 
      // Vector of the latest move input.
-     private Vector2 movementVector;
+     private float moveAxis = 0f;
 
-    // Start is called before the first frame update
-    void Start()
+     private void OnTriggerEnter2D(Collider2D collision)
+     {
+          if (collision.CompareTag("Platform"))
+          {
+               Debug.Log("Triggered!");
+               canJump = true;
+               jumpsLeft = numJumps;
+          }
+     }
+
+
+/*     private void OnTriggerExit2D(Collider2D collision)
+     {
+          if (collision.CompareTag("Platform"))
+          {
+               canJump = false;
+               Debug.Log(canJump);
+          }
+     }*/
+
+     // Start is called before the first frame update
+     void Start()
     {
           myRigidBody2D = GetComponent<Rigidbody2D>();
-          movementVector = Vector2.zero;
+          myCircleCollider2D = GetComponent<CircleCollider2D>();
+          jumpsLeft = numJumps;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-          myRigidBody2D.MovePosition(new Vector2(transform.position.x, transform.position.y) + movementVector);
-     }
+          Move();
+    }
 
 
+     // NOTE: This is mainly to GET THE INPUT CONTEXT FLOATS
      public void OnMove(InputAction.CallbackContext context)
      {
-          Move(context.ReadValue<Vector2>());
-        
+          moveAxis = context.ReadValue<float>();
      }
 
-     private void Move(Vector2 moveVector)
+     public void OnJump(InputAction.CallbackContext context)
      {
-          movementVector = moveVector * moveSpeed * Time.fixedDeltaTime;
-          //transform.position += new Vector3(movementVector.x, movementVector.y, 0);
-          //myRigidBody2D.MovePosition(new Vector2(transform.position.x, transform.position.y) + movementVector);
+          if (context.started &&
+               canJump == true && 
+               jumpsLeft > 0)
+          {
+               Jump();
+          }
+          
+     }
+
+     private void Jump()
+     {
+          Debug.Log("Jump!");
+          myRigidBody2D.velocity = Vector2.up * jumpHeight;
+          jumpsLeft--;
+     }
+
+     // Handle main move logic
+     private void Move()
+     {
+          Vector2 currentposition = transform.position;
+          currentposition.x += moveAxis * moveSpeed * Time.deltaTime;
+          transform.position = currentposition;
      }
 
 }
